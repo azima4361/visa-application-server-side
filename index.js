f6b6a73808a93ee9537a506e787c6bda35828bce
ssssss-code <128954534+ssssss-code@users.noreply.github.com>
2025-03-27 14:08:32 +0600

    patch the database

--- index.js
+++ index.js
@@ -55,7 +55,12 @@ async function run() {
     res.send(result);
 })
 
-
+app.get('/all/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await visaCollection.findOne(query);
+  res.send(result);
+})
 
     app.post('/all', async (req, res) => {
         const newVisa = req.body;
@@ -64,6 +69,56 @@ async function run() {
         const result = await visaCollection.insertOne(newVisa);
         res.send(result);
     });
+
+   
+    
+
+
+    // app.put('/all/:id', async (req, res) => {
+    //   const id = req.params.id;
+    //   const filter = { _id: new ObjectId(id) };
+    //   const options = { upsert: true };
+    //   const updatedDoc = {
+    //       $set: req.body
+    //   }
+    
+    //   const result = await visaCollection.updateOne(filter, updatedDoc, options )
+    
+    //   res.send(result);
+    // })
+
+    app.patch("/all/:id", async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const options = {upsert: true};
+      const updateVisa = req.body;
+      const Visa = {
+          $set: {
+              countryName: updateVisa.countryName,
+              countryImage: updateVisa.countryImage,
+              visaType: updateVisa.visaType,
+              processingTime: updateVisa.processingTime,
+              fee: updateVisa.fee,
+              validity: updateVisa.validity,
+              applicationMethod: updateVisa.applicationMethod,
+              
+          }
+      }
+      
+
+      const result = await visaCollection.updateOne(filter, Visa, options);
+      res.send(result);
+  })
+    
+    app.delete('/all/:id', async (req, res) => {
+      console.log('going to delete', req.params.id)
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await visaCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    
 app.get('/applications', async (req, res) => {
   const cursor = applicationCollection.find();
   const result = await cursor.toArray();
@@ -77,20 +132,52 @@ app.get('/applications', async (req, res) => {
       res.send(result);
   });
 
-  app.get('/applications/:email', async (req, res) => {
-  const userEmail = req.params.email;
-  const query = { email: userEmail };
+  app.get('/applications/email/:email', async (req, res) => {
+  const applicationEmail = req.params.email;
+  const query = { email: applicationEmail };
   const result = await applicationCollection.find(query).toArray();
   res.send(result);
 });
 
+app.get('/applications/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id : new ObjectId(id)};
+  const result = await applicationCollection.findOne(query);
+  res.send(result);
+});
+
+app.put('/applications/:id', async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) };
+  const options = { upsert: true };
+  const updatedDoc = {
+      $set: req.body
+  }
+
+  const result = await applicationCollection.updateOne(filter, updatedDoc, options )
+
+  res.send(result);
+})
 
 app.delete('/applications/:id', async (req, res) => {
+  console.log('going to delete', req.params.id)
   const id = req.params.id;
   const query = { _id: new ObjectId(id) };
   const result = await applicationCollection.deleteOne(query);
   res.send(result);
 });
+
+
+
+
+
+app.get('/all/email/:email', async (req, res) => {
+  const visaEmail = req.params.email;
+  const query = { email: visaEmail };
+  const result = await visaCollection.find(query).toArray();
+  res.send(result);
+})
+
 
 
 app.get('/users', async(req,res)=>{
